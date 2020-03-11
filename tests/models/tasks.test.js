4dfe41ddbca660894f1be5e681756c1b53207e27
theobenoit <theo.benoit16@gmail.com>
2020-03-11 16:25:23 +0100

    Créer les différents tests pour le modèle de tâche :
    -S’assurer que l’on ne puisse pas ajouter une tâche sans que le nom soit renseigné
    -S’assurer que la date de création par défaut soit bien celle lors de la création de la tâche
    -S’assurer que le statut par défaut soit “en attente”
    -S’assurer que le statut ne puisse rien être autre que en attente, en cours, complétée
    -S’assurer que si on remplit tout (statut, nom et date de création), on est bien tout qui soit rempli comme on l’a mis

--- tests/models/tasks.test.js
+++ tests/models/tasks.test.js
@@ -55,4 +55,75 @@ describe('Task Model Test', () => {
         expect(savedTask.created_at).toBeDefined();
     });
 
+    it('should not create a task if the name isn\'t filled', async () => {
+        const requiredNameTask = new Task({});
+
+        let err;
+
+        try {
+            const savedTaskWithoutRequiredName = await requiredNameTask.save();
+            err = savedTaskWithoutRequiredName;
+        } catch (error) {
+            err = error;
+        }
+
+        expect(err).toBeInstanceOf(mongoose.Error.ValidationError);
+        expect(err.errors.name).toBeDefined();
+    });
+
+    it('should create a task with the today date', async () => {
+        const validTask = new Task({
+            name: 'Le nom de ma première tâche'
+        });
+        const now = new Date(Date.now());
+        const savedTask = await validTask.save();
+
+        expect(savedTask.name).toBeDefined();
+        expect(savedTask.status).toBeDefined();
+        expect(savedTask.created_at).toEqual(now);
+    });
+
+    it('should create a task with by default pending as status', async () => {
+        const validTask = new Task({
+            name: 'Le nom de ma première tâche'
+        });
+        const savedTask = await validTask.save();
+
+        expect(savedTask.name).toBeDefined();
+        expect(JSON.stringify(savedTask.status)).toEqual(JSON.stringify(['pending']));
+        expect(savedTask.created_at).toBeDefined()
+    });
+
+    it('should not be possible to create a task with anything than pending, ongoing or completed as status', async () => {
+        const statusIsNotValidTask = new Task({
+            name: 'Le nom de ma première tâche',
+            status: 'Ce statut n\'est pas valide'
+        });
+
+        let err;
+
+        try {
+            const savedTaskWithoutValidStatus = await statusIsNotValidTask.save();
+            err = savedTaskWithoutValidStatus;
+        } catch (error) {
+            err = error;
+        }
+
+        expect(err).toBeInstanceOf(mongoose.Error.ValidationError);
+    });
+
+    it('should create a task with the informations we filled in', async () => {
+        const createdAt = new Date(new Date().getMonth() + 1);
+        const validTask = new Task({
+            name: 'Le nom de ma première tâche',
+            status: 'ongoing',
+            created_at: createdAt
+        });
+        const savedTask = await validTask.save();
+
+        expect(savedTask.name).toBe('Le nom de ma première tâche');
+        expect(JSON.stringify(savedTask.status)).toEqual(JSON.stringify(['ongoing']));
+        expect(savedTask.created_at).toEqual(createdAt);
+    });
+
 });
